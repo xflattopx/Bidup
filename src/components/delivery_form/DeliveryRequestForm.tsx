27132716cfd3964f0f47d1d63ac4790fffc052bb
Steven Charles <iwritecodes@protonmail.com>
2023-12-23 15:10:17 -0500

    endpoint updates

--- src/components/delivery_form/DeliveryRequestForm.tsx
+++ src/components/delivery_form/DeliveryRequestForm.tsx
@@ -50,7 +50,7 @@ const DeliveryRequestForm: React.FC<DeliveryRequestFormProps> = () => {
       // Dispatching the action is commented out for now, as the axios.post call is used directly
       // dispatch(await submitDeliveryRequest(formState));
 
-      axios.post(`${apiUrl}/customer_request`, formState);
+      axios.post(`${apiUrl}/service/`, formState);
 
       setFormState({
         pickupLocation: '',
